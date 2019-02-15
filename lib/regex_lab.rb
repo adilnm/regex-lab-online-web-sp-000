b186e67bc4cc58735e8bd03136e549717beef3f9
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-15 02:02:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,5 +19,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+if(phone.match(/\(?\d{3}\)?\s?\d{3}\-?\s?\d{4}/)==nil)
+  return false
+else 
+  return true
 end
